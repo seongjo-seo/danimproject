0c59569836f53205acd7f34d7aaa8039e5b78341
seoseongjo <tjtjdwh5321@gmail.com>
2022-09-17 11:30:17 +0900

    mysql 연동

---
+++ danim/danim/my_settings.py
@@ -0,0 +1,12 @@
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'danim',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
